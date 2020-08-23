c4fb3ead1759ced416651ab38726ec7349580989
C137 <ali103575@gmail.com>
2020-08-24 01:11:55 +0300

    Some Fixes

--- Articles/Stress Testing/good implementation using diff/script.sh
+++ Articles/Stress Testing/good implementation using diff/script.sh
@@ -6,7 +6,7 @@ for((i = 1; ; ++i)); do
     ./gen $i > input_file
     ./code < input_file > myAnswer
     ./brute < input_file > correctAnswer
-    diff -w myAnswer correctAnswer > /dev/null || break
+    diff -Z myAnswer correctAnswer > /dev/null || break
     echo "Passed test: "  $i
 done
 echo "WA on the following test:"
